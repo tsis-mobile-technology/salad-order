60772d6a2dc4b0ee74cce6821a3c992fb668453f
taejonggo <gainworld@gmail.com>
2024-08-01 00:26:39 +0900

    in home

--- src/main/java/com/tsis/hello/SpringConfig.java
+++ src/main/java/com/tsis/hello/SpringConfig.java
@@ -1,14 +1,29 @@
 package com.tsis.hello;
 
+import com.tsis.hello.repository.JdbcMemberRepository;
 import com.tsis.hello.repository.MemberRepository;
 import com.tsis.hello.repository.MemoryMemberRepository;
 import com.tsis.hello.service.MemberService;
+import com.tsis.hello.domain.Member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.util.List;
+import java.util.Optional;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +32,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
